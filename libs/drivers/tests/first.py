1b80eb1b80cd1aa5808972af4da872fc778ee7c9
zhao <20024592+agentzhao@users.noreply.github.com>
2026-02-06 11:56:05 +0800

    fixed

--- libs/drivers/tests/first.py
+++ libs/drivers/tests/first.py
@@ -19,34 +19,34 @@ if __name__ == "__main__":
         sartorius_port="/dev/ttyUSB0",
         camera_index=0,
     )
-    
-    # View available labware
-    print(get_available_labware())
-
-    # Define deck layout declaratively and load all at once
-    machine.load_deck(
-        {
-            "C1": "trash_bin",
-            "C2": "polyelectric_8_wellplate_30000ul",
-            "A3": "opentrons_96_tiprack_300ul",
-        }
-    )
-
-    print(machine.deck)
-    print(machine.deck["C2"])
-
     machine.startup()  # Connects all controllers, homes gantry, and initializes pipette
     
-    machine.get_absolute_a_position(slot="A3", well="A1")
-    # machine.move_electrode(slot="A3", well="A1", height_from_bottom=0)
+    # # View available labware
+    # print(get_available_labware())
+
+    # # Define deck layout declaratively and load all at once
+    # machine.load_deck(
+    #     {
+    #         "C1": "trash_bin",
+    #         "C2": "polyelectric_8_wellplate_30000ul",
+    #         "A3": "opentrons_96_tiprack_300ul",
+    #     }
+    # )
+
+    # print(machine.deck)
+    # print(machine.deck["C2"])
+
     
-    # machine.record_video(duration_seconds=10, filename="test.mp4")
-    machine.start_video_recording()
-    machine.attach_tip(slot="A3", well="G8")
-    machine.aspirate_from(slot="C2", well="A1", amount=100, height_from_bottom=10)
-    # machine.capture_image()
-    machine.dispense_to(slot="C2", well="B4", amount=100, height_from_bottom=50)
-    machine.drop_tip(slot="C1", well="A1", height_from_bottom=10)
+    # machine.get_absolute_a_position(slot="A3", well="A1")
+    # # machine.move_electrode(slot="A3", well="A1", height_from_bottom=0)
+    
+    # # machine.record_video(duration_seconds=10, filename="test.mp4")
+    # machine.start_video_recording()
+    # machine.attach_tip(slot="A3", well="G8")
+    # machine.aspirate_from(slot="C2", well="A1", amount=100, height_from_bottom=10)
+    # # machine.capture_image()
+    # machine.dispense_to(slot="C2", well="B4", amount=100, height_from_bottom=50)
+    # machine.drop_tip(slot="C1", well="A1", height_from_bottom=10)
     
     # tiprack_wells = machine.deck["A3"].wells
     # # get pick up pipette one by one and drop it in the trash bin
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     #     machine.drop_tip("C1", "A1")
 
 
-    machine.stop_video_recording()
+    # machine.stop_video_recording()
     
     # Shutdown machine
     machine.shutdown()
